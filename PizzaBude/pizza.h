01f9c17990765deaf41a3ae07eacb61c5b064b9d
Péter Gyarmati <gyarmatipeti01@gmail.com>
2020-01-19 13:58:14 +0100

    PizzaBude initial commit - broken main as classes are not implemented yet

---
+++ PizzaBude/pizza.h
@@ -0,0 +1,14 @@
+//
+// Created by Peter Gyarmati on 2020. 01. 19..
+//
+
+#ifndef PIZZABUDE_PIZZA_H
+#define PIZZABUDE_PIZZA_H
+
+
+class Pizza {
+
+};
+
+
+#endif //PIZZABUDE_PIZZA_H
---
+++ PizzaBude/topping.h
@@ -0,0 +1,14 @@
+//
+// Created by Peter Gyarmati on 2020. 01. 19..
+//
+
+#ifndef PIZZABUDE_TOPPING_H
+#define PIZZABUDE_TOPPING_H
+
+
+class Topping {
+
+};
+
+
+#endif //PIZZABUDE_TOPPING_H
