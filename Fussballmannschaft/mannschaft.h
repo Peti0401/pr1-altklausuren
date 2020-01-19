dbb235aaabca2763b95c9c6970ce9204c63d3a0c
Péter Gyarmati <gyarmatipeti01@gmail.com>
2020-01-19 08:29:21 +0100

    Fussballmannschaft initial commit - broken main as classes are not implemented yet

---
+++ Fussballmannschaft/mannschaft.h
@@ -0,0 +1,14 @@
+//
+// Created by Peter Gyarmati on 2020. 01. 19..
+//
+
+#ifndef FUSSBALLMANNSCHAFT_MANNSCHAFT_H
+#define FUSSBALLMANNSCHAFT_MANNSCHAFT_H
+
+
+class Mannschaft {
+
+};
+
+
+#endif //FUSSBALLMANNSCHAFT_MANNSCHAFT_H
---
+++ Fussballmannschaft/spieler.h
@@ -0,0 +1,14 @@
+//
+// Created by Peter Gyarmati on 2020. 01. 19..
+//
+
+#ifndef FUSSBALLMANNSCHAFT_SPIELER_H
+#define FUSSBALLMANNSCHAFT_SPIELER_H
+
+class Spieler {
+
+};
+
+
+
+#endif //FUSSBALLMANNSCHAFT_SPIELER_H
