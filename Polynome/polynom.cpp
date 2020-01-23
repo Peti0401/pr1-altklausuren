614f4f94a93851df1ca2840a2e38b9de8e6e51b5
Péter Gyarmati <gyarmatipeti01@gmail.com>
2020-01-24 00:08:41 +0100

    Polynome initial commit - broken main as classes are not implemented

---
+++ Polynome/polynom.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Peter Gyarmati on 2020. 01. 24..
+//
+
+#include "polynom.h"
---
+++ Polynome/polynom.h
@@ -0,0 +1,20 @@
+//
+// Created by Peter Gyarmati on 2020. 01. 24..
+//
+
+#ifndef POLYNOME_POLYNOM_H
+#define POLYNOME_POLYNOM_H
+
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+
+class Polynom {
+
+};
+
+
+#endif //POLYNOME_POLYNOM_H
